0bc1328b251eb16cf16903d05d99f632f75996fa
BRB3D <jislaseng@gmail.com>
2022-01-14 17:45:57 -0500

    style for navbar

--- Client/devs-den/src/components/NavBar.jsx
+++ Client/devs-den/src/components/NavBar.jsx
@@ -5,10 +5,16 @@ import "./Styles/NavBar.scss";
 
 function NavBar() {
   return (
-    <div id="navbar">
+    <nav  className="navbar navbar-light bg-light">
       <div className="navbar-user"></div>
-      <h1>Hello World!</h1>
-    </div>
+      <div className="collapse navbar-collapse">
+        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div> 
+      <div className="navbar-login/logout"><h3>Login | Logout</h3></div> 
+    </nav>
   );
 }
 
