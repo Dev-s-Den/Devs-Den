b88db9e1b6203e7d3d45fb9b76e564bc09bc622f
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-14 16:56:46 -0500

    Implement basic navbar

---
+++ Client/devs-den/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styles
+import "./Styles/NavBar.scss";
+
+function NavBar() {
+  return (
+    <div id="navbar">
+      <div className="navbar-user"></div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default NavBar;
