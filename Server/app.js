889762bac6efe714be190c4273f0b3cc246248ca
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-16 17:10:02 -0500

    enviroment and dependencies setup

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -11,7 +12,10 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: process.env.SESSION_NAME,
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
