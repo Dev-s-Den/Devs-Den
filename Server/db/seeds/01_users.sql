b52fc7cb0946f547ac4532723ff00bb05f368708
BRB3D <jislaseng@gmail.com>
2022-01-15 22:01:32 -0500

    users seeds

---
+++ Server/db/seeds/01_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (avatar, first_name, last_name, email, password, github_url)
+ VALUES ('Alice', 'Alex', 'food', 'eating@food.com', "1", 'github1'),
+        ('avatar1', 'No Way', "Jose", 'coding@email.com'," 2", 'github2'), 
+        ('avatar2', 'Nicholas', 'Alk', 'bot@discord.com', "3", 'github3')
+
+
+
