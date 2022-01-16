5aee539d12fd36360ca90d60a15cd562ba21f6ac
BRB3D <jislaseng@gmail.com>
2022-01-16 10:56:14 -0500

    Typos for the schema and seeds

--- Server/db/seeds/01_users.sql
+++ Server/db/seeds/01_users.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (avatar, first_name, last_name, email, password, github_url)
- VALUES ('https://i.imgur.com/QAus8RQ.png', 'Alex', 'food', 'eating@food.com', "1", 'github1'),
-        ('https://i.imgur.com/QAus8RQ.png', 'No Way', "Jose", 'coding@email.com'," 2", 'github2'), 
-        ('https://i.imgur.com/QAus8RQ.png', 'Nicholas', 'Alk', 'bot@discord.com', "3", 'github3')
+INSERT INTO users (id, avatar, first_name, last_name, email, password, github_url)
+ VALUES (1,'https://i.imgur.com/QAus8RQ.png', 'Alex', 'food', 'eating@food.com', '1', 'github1'),
+        (2,'https://i.imgur.com/QAus8RQ.png', 'No Way', 'Jose', 'coding@email.com','1', 'github2'), 
+        (3,'https://i.imgur.com/QAus8RQ.png', 'Nicholas', 'Alk', 'bot@discord.com', '1', 'github3')
 
 
 
--- Server/db/seeds/02_posts.sql
+++ Server/db/seeds/02_posts.sql
@@ -1,6 +1,6 @@
-INSERT INTO posts (user_id, forum_id, content, img)
- VALUES ('1', '2', 'Someone please explain how to create methods inside of a ruby object?', 'https://i.imgur.com/fuOFQoH.png'),
-        ('2', '1', "How do I create a branch and checkout at the same time? ", 'https://i.imgur.com/fuOFQoH.png'), 
-        ('3', '2', 'How did Jose Alex and Nico make this amazing app?', 'https://i.imgur.com/fuOFQoH.png')
+INSERT INTO posts (id, user_id, forum_id, content, img)
+ VALUES (1,'1', '2', 'Someone please explain how to create methods inside of a ruby object?', 'https://i.imgur.com/fuOFQoH.png'),
+        (2,'2', '1', 'How do I create a branch and checkout at the same time? ', 'https://i.imgur.com/fuOFQoH.png'), 
+        (3,'3', '2', 'How did Jose Alex and Nico make this amazing app?', 'https://i.imgur.com/fuOFQoH.png')
 
 
