0e220a09fc3039b429cbd062f7c2e743b346e45c
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-15 22:15:40 -0500

    comments seeds

---
+++ Server/db/seeds/03_comments.sql
@@ -0,0 +1,4 @@
+INSERT INTO comments (post_id, user_id, content)
+ VALUES ('1', '2', 'I have no idea how to help you.'),
+        ('1', '1', "This is an amazing question but maybe someone with more expereince can help."), 
+        ('2', '3', 'No Way Jose will be able to help you with this')
