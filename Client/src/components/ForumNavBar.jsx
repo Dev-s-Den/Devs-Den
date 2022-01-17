d620a6ad5b095899ae7fb79093ff7877a21a3522
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-17 13:42:18 -0500

    forum navbar styling

--- Client/src/components/ForumNavBar.jsx
+++ Client/src/components/ForumNavBar.jsx
@@ -4,12 +4,17 @@ import "./Styles/ForumNavBar.scss";
 
 export default function ForumNavBar() {
 
+  return (
+    <nav className="forumnav">
 
+      <div className="forumnav-container">
+        <a href=""><img src="https://symbols.getvecta.com/stencil_25/39_javascript.0ca26ec4ab.svg"></img></a>
+      </div>
 
+      <div className="forumnav-container">
+        <a href=""><img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/120px-ISO_C%2B%2B_Logo.svg.png"></img></a>
+      </div>
 
-  return(
-    <h1>
-      Hello There
-    </h1>
+    </nav>
   )
 }
