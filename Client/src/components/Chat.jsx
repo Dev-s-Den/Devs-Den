674bae8b4a740a66d91daaa0c76ce34484955699
BRB3D <jislaseng@gmail.com>
2022-01-16 20:51:31 -0500

    chat test components separatred and working

--- Client/src/components/Chat.jsx
+++ Client/src/components/Chat.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { ChatEngineWrapper, Socket, ChatList, ChatFeed, ChatSettings } from 'react-chat-engine'
 
 import { Col } from 'react-grid-system'
 
 const Chat = () => {
+
+const [selector, setSelector] = useState('feed')
+
+
     return (
-        <ChatEngineWrapper>
+        <ChatEngineWrapper    >
             <Socket 
-                projectID={process.env.REACT_APP_KEY}
-                userName='test'
-                userSecret='secret'
+               projectID='fd57cd2d-03c6-4bda-ab02-2223fd1adc39'
+               userName='test'
+               userSecret='test'
             />
 
-            <Col xs={0} sm={3}>
-                <ChatList />
-            </Col>
+          { selector === 'list' &&  <Col xs={0} sm={3}>
+                <ChatList  projectID='fd57cd2d-03c6-4bda-ab02-2223fd1adc39'
+        userName='test'
+        userSecret='test' />
+              
+            </Col>}
 
-            <Col xs={12} sm={6}>
+            {selector === 'feed' && <Col xs={12} sm={6}>
                 <ChatFeed />
-            </Col>
+            </Col>}
 
-            <Col xs={0} sm={3}>
+           {selector === 'settings' &&  <Col xs={0} sm={3}>
                 <ChatSettings />
-            </Col>
+            </Col>}
         </ChatEngineWrapper>
+
+        
     )
 }
 
