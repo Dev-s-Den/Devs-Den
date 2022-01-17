cd005bc23dd5fff9542079ead5f828f77f7d2764
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-16 19:02:36 -0500

    failed connection

--- Server/app.js
+++ Server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: process.env.SESSION_NAME,
   keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
