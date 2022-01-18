56ed11a0f7403835ca182e4dc0db32d42bd91f64
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-18 12:11:10 -0500

    pop up modal for chat view

--- Client/src/components/Chat.jsx
+++ Client/src/components/Chat.jsx
@@ -1,16 +1,19 @@
 import React from "react"
-
+import { ChatEngine } from 'react-chat-engine';
 import "./Styles/Chat.scss"
 
 export default function Chat(props) {
 
- 
-
+ console.log(process.env.REACT_APP_chatProjectID)
   return(
     <div className={`modal-chat-exterior ${props.show ? 'show': ''}`} onClick={props.closeModal}>
       
       <div className="modal-chat-body" onClick={e => e.stopPropagation()}>
-        Body Of THE MODAL
+        <ChatEngine
+          projectID= {process.env.REACT_APP_chatProjectID}
+          height= "60vh" userName={process.env.REACT_APP_chatUsername}
+          userSecret={process.env.REACT_APP_chatUserSecret}
+        />
         <div className="modal-chat-footer">
           <button onClick={props.closeModal}>CLOSE MODAL</button>
         </div>
