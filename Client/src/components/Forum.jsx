b90a97c985519537ed6bffbef96925930d6804b8
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-17 20:34:29 -0500

    create files

---
+++ Client/src/components/Forum.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Forum.scss"
+
+export default function Forum() {
+  return (
+    <div>
+      <img src="https://getflywheel.com/layout/wp-content/uploads/2019/02/The_Best_Java_Script_Libraries_1800x500-1-1568x436.jpg" alt="JS BANNER" />
+    </div>
+  )
+}
