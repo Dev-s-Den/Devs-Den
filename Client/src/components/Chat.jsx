a87d00a917c153ff4fcf21cc2d1c1f9cf2ae561a
BRB3D <jislaseng@gmail.com>
2022-01-18 14:39:00 -0500

    Chat fix from merge

--- Client/src/components/Chat.jsx
+++ Client/src/components/Chat.jsx
@@ -3,14 +3,12 @@ import { ChatEngine } from 'react-chat-engine';
 import "./Styles/Chat.scss"
 
 export default function Chat(props) {
-
- console.log(process.env.REACT_APP_chatProjectID)
   return(
     <div className={`modal-chat-exterior ${props.show ? 'show': ''}`} onClick={props.closeModal}>
       
       <div className="modal-chat-body" onClick={e => e.stopPropagation()}>
         <ChatEngine
-          projectID= {process.env.REACT_APP_chatProjectID}
+          projectID={process.env.REACT_APP_chatProjectID}
           height= "60vh" userName={process.env.REACT_APP_chatUsername}
           userSecret={process.env.REACT_APP_chatUserSecret}
         />
