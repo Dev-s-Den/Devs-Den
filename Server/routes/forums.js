54a7a6b098b0236cbc91cb25961e895d61c92f6d
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-18 17:02:42 -0500

    add new route and query for forum_id

--- Server/routes/forums.js
+++ Server/routes/forums.js
@@ -6,5 +6,8 @@ module.exports = () => {
   router.get('/', (req, res) => {
     getForums().then(data => res.json(data)).catch(err => console.error(err.message))
   })
+  router.get('/:forum_id',(req,res) => {
+    getForumById(req.params.forum_id).then(data => res.json(data)).catch(err => console.error(err.message))
+  } )
   return router
 }
