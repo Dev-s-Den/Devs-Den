e792a20e770aa392b3500b6ca81ddcce5ba7cb3c
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-17 21:28:12 -0500

    basic formating complete

--- Client/src/components/Forum.jsx
+++ Client/src/components/Forum.jsx
@@ -1,10 +1,27 @@
 import React from "react"
-import "./Forum.scss"
+import "./Styles/Forum.scss"
+import Post from "./Post.jsx"
 
 export default function Forum() {
+
+  const foruminfo = {
+    image: "https://getflywheel.com/layout/wp-content/uploads/2019/02/The_Best_Java_Script_Libraries_1800x500-1-1568x436.jpg",
+    name: "JavaScript"
+  }
+
+
   return (
-    <div>
-      <img src="https://getflywheel.com/layout/wp-content/uploads/2019/02/The_Best_Java_Script_Libraries_1800x500-1-1568x436.jpg" alt="JS BANNER" />
-    </div>
+    <section>
+      <div className="forum-name">
+        <h1 ><em>{foruminfo.name}</em></h1>
+      </div>
+      <div>
+        <img className= "forum-banner" src={foruminfo.image} alt={foruminfo.name} />
+      </div>
+
+      <div className="forum-postContainer">
+
+      </div>
+    </section>
   )
 }
