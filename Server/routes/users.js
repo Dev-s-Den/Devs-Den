f521fa9709455964ee101769b1e3d7a540689643
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-19 15:07:12 -0500

    add route to check for user logged in using cookie

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -23,8 +23,20 @@ module.exports = () => {
         if (!bcrypt.compareSync(req.body.password, data[0].password)) {
           return res.send('Incorrect credentials').status(403);
         }
-        res.send({cookie: req.session.user_id = data[0].user_id, user_id: data[0].id, avatar: data[0].avatar, first_name: data[0].first_name, last_name: data[0].last_name, email: data[0].email, github_url: data[0].github_url })
+        res.send({cookie: req.session.email = data[0].email, user_id: data[0].id, avatar: data[0].avatar, first_name: data[0].first_name, last_name: data[0].last_name, email: data[0].email, github_url: data[0].github_url })
       })
+  })
+
+  router.get('/checkLogin', (req,res) => {
+    const user_email = req.session.email
+    if(user_email){
+      getUsers(user_email).then(data => {
+        res.json({user_id: data[0].id, avatar: data[0].avatar, first_name: data[0].first_name, last_name: data[0].last_name, email: data[0].email, github_url: data[0].github_url})
+      })
+    } 
+    else {
+      return res.status(403).send('Not Logged In')
+    }
   })
 
   return router;
