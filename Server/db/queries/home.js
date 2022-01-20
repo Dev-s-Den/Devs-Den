8e919bf2db1aba7665485baf96ee55080ab032e4
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-20 11:19:05 -0500

    Add user_id to the homepage query

--- Server/db/queries/home.js
+++ Server/db/queries/home.js
@@ -14,7 +14,7 @@ const getHomePosts = async () => {
   try {
     let posts = [];
     for (const value of values) {
-      const data = await dbConnection.query(`SELECT posts.id, to_char(posts.created_at  :: Date, 'Mon dd, yy HH12:MI') AS created_at , first_name, last_name, avatar, content, img FROM posts JOIN users ON users.id = posts.user_id WHERE forum_id=$1 LIMIT 3`, [value]);
+      const data = await dbConnection.query(`SELECT posts.id, to_char(posts.created_at  :: Date, 'Mon dd, yy HH12:MI') AS created_at , user_id, first_name, last_name, avatar, content, img FROM posts JOIN users ON users.id = posts.user_id WHERE forum_id=$1 LIMIT 3`, [value]);
       posts = [...posts, ...data.rows];
     }
     return posts;
