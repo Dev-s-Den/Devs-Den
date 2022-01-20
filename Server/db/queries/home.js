184b2c5db6389f10936485377334b1cbf974c30a
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-20 09:40:00 -0500

    Fix homepage query and route

--- Server/db/queries/home.js
+++ Server/db/queries/home.js
@@ -1,10 +1,23 @@
 const dbConnection = require('../connection');
+const { getForums } = require('./forums');
 
-const getHomePosts = async (query_id) => {
-  const values = [query_id];
+const filterForums = async () => {
+  let idArray = [];
+  await getForums().then(forums => {
+    forums.forEach(forum => idArray.push(forum.id));
+  })
+  return idArray;
+};
+
+const getHomePosts = async () => {
+  const values = await filterForums();
   try {
-    const data = await dbConnection.query(`SELECT * FROM posts WHERE forum_id=$1 LIMIT 3`, values);
-    return data.rows;
+    let posts = [];
+    for (const value of values) {
+      const data = await dbConnection.query(`SELECT * FROM posts WHERE forum_id=$1 LIMIT 3`, [value]);
+      posts = [...posts, ...data.rows];
+    }
+    return posts;
   } catch (err) {
     console.error(err.message);
     return err.message;
--- Server/routes/home.js
+++ Server/routes/home.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { getHomePosts } = require('../db/queries/home');
 
 module.exports = () => {
-  router.get('/:forum_id', (req, res) => {
-    getHomePosts(req.params.forum_id)
+  router.get('/', (req, res) => {
+    getHomePosts()
       .then(data => res.json(data));
   })
   return router;
