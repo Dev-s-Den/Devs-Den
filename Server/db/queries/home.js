4e6873e4b1dd2eefa9ebb549f3740863c3623a4e
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-19 22:42:15 -0500

    Fix homepage query

--- Server/db/queries/home.js
+++ Server/db/queries/home.js
@@ -1,26 +1,14 @@
 const dbConnection = require('../connection');
-const { getForums } = require('./forums');
 
-const getHomePosts = async () => {
-  await getForums().then(forums => {
-    let topPosts = [];
-    try {
-      for (const forum of forums) {
-        const values = Object.values(forum)[0];
-        dbConnection.query('SELECT * FROM posts WHERE forum_id = $1 LIMIT 3;', [values])
-          .then(data => {
-            topPosts = [...topPosts, data.rows];
-          })
-      }
-      return console.log(topPosts);
-    } catch (err) {
-      console.error(err.message);
-      return err.message;
-    }
-  })
+const getHomePosts = async (query_id) => {
+  const values = [query_id];
+  try {
+    const data = await dbConnection.query(`SELECT * FROM posts WHERE forum_id=$1 LIMIT 3`, values);
+    return data.rows;
+  } catch (err) {
+    console.error(err.message);
+    return err.message;
+  }
 }
-
-getHomePosts()
-
 
 module.exports = { getHomePosts }
