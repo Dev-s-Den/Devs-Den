1a256b89c03520137beec42e1eaa21e1b4a3dc75
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-20 10:59:03 -0500

    add chat_engine_secret to db

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -7,6 +7,16 @@ const { getUsers, addUsers, checkUser } = require('../db/queries/users');
 const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session');
 
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
 
 module.exports = () => {
   router.get('/', (req, res) => {
@@ -16,7 +26,7 @@ module.exports = () => {
   router.post('/', (req, res) => {
     let avatar = `https://avatars.dicebear.com/api/bottts/${req.body.email}.svg`
     const hashPassword = bcrypt.hashSync(req.body.password, 10)
-    addUsers(avatar, req.body.first_name, req.body.last_name, req.body.email, hashPassword, req.body.github_url)
+    addUsers(avatar, req.body.first_name, req.body.last_name, req.body.email, makeid(16), hashPassword, req.body.github_url)
       .then(data => {
         res.send({cookie: req.session.email = data[0].email, user_id: data[0].id, avatar: data[0].avatar, first_name: data[0].first_name, last_name: data[0].last_name, email: data[0].email, github_url: data[0].github_url })
       })
