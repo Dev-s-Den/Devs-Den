87695207417a0cccb99bbe76c5fdd52b13eef14b
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-20 11:26:43 -0500

    Make a post requests to update comments in db

--- Client/src/components/Post.jsx
+++ Client/src/components/Post.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 // Styles
 import "./Styles/Post.scss";
@@ -10,37 +10,44 @@ import Comment from "./Comment.jsx";
 export default function Post(props) {
   // States
   const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
-
     Promise.all([
-      axios.get(`/api/comments/${props.id}`)
+      axios.get(`/api/comments/${props.id}`),
+      axios.post(`api/comments/${props.id}`),
     ]).then((all) => {
-      setComments( all[0].data);
+      setComments(all[0].data);
     });
-  }, [])
+  }, []);
 
-  const [showComments, setShowComments] = useState(false);
   const switchCommentShow = () => setShowComments(showComments ? false : true);
-
-  const [comment, setComment] = useState("");
   const submitComment = () => {
-    setComments(prev => [...prev, comment]);
+    setComments((prev) => [
+      ...prev,
+      {
+        user_id: props.user_id,
+        post_id: props.id,
+        content: comment,
+      },
+    ]);
     setComment("");
   };
-
 
   return (
     <div className="post">
       <header className="post--header">
         <div className="post--header--left">
-          <img className="post--avatar" src={`${props.avatar}`} alt="user-avatar"/>
+          <img
+            className="post--avatar"
+            src={`${props.avatar}`}
+            alt="user-avatar"
+          />
           <h2 className="post--name">{props.first_name}</h2>
         </div>
         <div className="post--header--left">
-          <small className="post--Time">
-          {props.created_at}
-          </small>
+          <small className="post--Time">{props.created_at}</small>
         </div>
       </header>
 
@@ -58,33 +65,35 @@ export default function Post(props) {
 
       <footer className="post--footer">
         <div className="post--footer--left">
-          <img className="post--user--avatar"/>
+          <img className="post--user--avatar" />
           <form
-          className='new-comment'
+            className="new-comment"
             onSubmit={(event) => {
               event.preventDefault();
               submitComment();
             }}
           >
-              <input
-                type="text"
-                className="new-comment-input"
-                id="submitNewComment"
-  
-                placeholder="Wite a comment..."
-                onChange={(event) => setComment(event.target.value)}
-                value={comment}
-              />
-              <button className="btn btn-m btn-success" type="submit">Submit</button>
+            <input
+              type="text"
+              className="new-comment-input"
+              id="submitNewComment"
+              placeholder="Wite a comment..."
+              onChange={(event) => setComment(event.target.value)}
+              value={comment}
+            />
+            <button className="btn btn-m btn-success" type="submit">
+              Submit
+            </button>
           </form>
         </div>
 
         <span className="post--actions">
-          <span className='hover'>
+          <span className="hover">
             <i className="far fa-thumbs-up"></i>7
           </span>
-          <span className='hover' onClick={switchCommentShow}>
-            <i className="far fa-comments"></i>{comments.length}
+          <span className="hover" onClick={switchCommentShow}>
+            <i className="far fa-comments"></i>
+            {comments.length}
           </span>
         </span>
       </footer>
