2c80f25281ad45d87df7942f59217fecc91d21b0
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-21 19:47:47 -0500

    add username in the backend

--- Server/db/seeds/01_users.sql
+++ Server/db/seeds/01_users.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (avatar, first_name, last_name, email, chat_engine_secret, password, github_url)
- VALUES ('https://i.imgur.com/QAus8RQ.png', 'Alex', 'food', 'eating@food.com','qMT60kDgChTIA33E', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github1'),
-        ('https://i.imgur.com/QAus8RQ.png', 'No Way', 'Jose', 'coding@code.com','Xgzq5YYdJGddFga1','$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github2'), 
-        ('https://i.imgur.com/QAus8RQ.png', 'Nicholas', 'Alk', 'bot@discord.com','8zZC1lkc4zSBYO41', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github3')
+INSERT INTO users (avatar,username, first_name, last_name, email, chat_engine_secret, password, github_url)
+ VALUES ('https://i.imgur.com/QAus8RQ.png','AB', 'Alex', 'food', 'eating@food.com','qMT60kDgChTIA33E', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github1'),
+        ('https://i.imgur.com/QAus8RQ.png','AB1', 'No Way', 'Jose', 'coding@code.com','Xgzq5YYdJGddFga1','$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github2'), 
+        ('https://i.imgur.com/QAus8RQ.png','AB2', 'Nicholas', 'Alk', 'bot@discord.com','8zZC1lkc4zSBYO41', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github3')
 
 
 
