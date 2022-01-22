cb6cd619fd331ebaca03ada0134d18e54fe97865
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-21 21:57:59 -0500

    fix users for chat engine

--- Server/db/seeds/01_users.sql
+++ Server/db/seeds/01_users.sql
@@ -1,7 +1,13 @@
 INSERT INTO users (avatar,username, first_name, last_name, email, chat_engine_secret, password, github_url)
- VALUES ('https://i.imgur.com/QAus8RQ.png','AB', 'Alex', 'food', 'eating@food.com','qMT60kDgChTIA33E', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github1'),
-        ('https://i.imgur.com/QAus8RQ.png','AB1', 'No Way', 'Jose', 'coding@code.com','Xgzq5YYdJGddFga1','$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github2'), 
-        ('https://i.imgur.com/QAus8RQ.png','AB2', 'Nicholas', 'Alk', 'bot@discord.com','8zZC1lkc4zSBYO41', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'github3')
+ VALUES ('https://avatars.dicebear.com/api/bottts/dsfdsfdsf.svg','Runningcharm', 'Alex', 'Raffa', 'alex@hotmail.com','UUHl0Jy529Zx2MVj', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'araff13'),
+        ('https://avatars.dicebear.com/api/bottts/sfdsgfgjgh.svg','Cosmox', 'Nico', 'Alkhoury', 'nico@hotmail.com','2JMjykTsnPZt4bI4','$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'nicalk4'), 
+        ('https://avatars.dicebear.com/api/bottts/jjkdhfjklg.svg','Everdev', 'Jose', 'Islas', 'jose@hotmail.com','8zZC1lkc4zSBYO41', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'josisl34'),
+        ('https://avatars.dicebear.com/api/bottts/iluiljxklhnds.svg','Coindev', 'Henry', 'Jacobs', 'henry@hotmail.com','Iyhr1YW8bALMZs86', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'henrcoin45'),
+        ('https://avatars.dicebear.com/api/bottts/ihiuhjsdf.svg','Advdeck', 'Johnny', 'Birk', 'johnny@hotmail.com','ILPw6923yEv8jNFJ', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'johnbirb3'),
+        ('https://avatars.dicebear.com/api/bottts/ioueuwriojk.svg','Sparkadv', 'Terry', 'Cook', 'terry@hotmail.com','QLZcWJi6rr7Tdtj9', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'terrvok'),
+        ('https://avatars.dicebear.com/api/bottts/imzs.svg','Coinhold', 'Harriot', 'Tub', 'harriot@hotmail.com','WVhROZ7Zha1q76LK', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'har4'),
+        ('https://avatars.dicebear.com/api/bottts/iurewjxz.svg','Tireloop', 'Nick', 'Pardon', 'pardon@hotmail.com','RUbiwcGjCGGh4TRi', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'nickpar4'),
+        ('https://avatars.dicebear.com/api/bottts/ewiouhjkl.svg','Sondevscape', 'Trevor', 'Phillips', 'trevor@hotmail.com','nC4bypukkhdBrdGU', '$2a$10$U5P/7IXhKpwA4GdQTCtK5ORUVqFeDK/LebxnBBCgwyKEwwEJ1wqBq', 'trevvy67')
 
 
 
