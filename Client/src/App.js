d3ca3fdb1679005f6c1e35b86412591582cac503
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-22 20:36:46 -0500

    add social media links and restyle

--- Client/src/App.js
+++ Client/src/App.js
@@ -85,25 +85,39 @@ function App() {
           <ul>
             <li>
               <h4>Alex Raffa</h4>
-              <div className="social-container">
-                <ion-icon name="logo-github" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-                <ion-icon name="logo-linkedin" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-              </div>
+              <section className="social-container">
+                <a className="github" href="https://github.com/araff-16">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a className="linkedIn" href="https://www.linkedin.com/in/alex-raffa/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+              </section>
             </li>
             <li>
               <h4>Jose Islas</h4>
-              <div className="social-container">
-                <ion-icon name="logo-github" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-                <ion-icon name="logo-linkedin" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-              </div>
+              <section className="social-container">
+                <a className="github" href="https://github.com/BRB3D">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a className="linkedIn" href="https://www.linkedin.com/in/jaia/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+              </section>
             </li>
             <li>
               <h4>Nicolas Alkhoury</h4>
-              <div className="social-container">
-                <ion-icon name="logo-github" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-                <ion-icon name="logo-linkedin" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-                <ion-icon name="logo-twitter" style={{ width: "55%", height: "55%", color: "white" }}></ion-icon>
-              </div>
+              <section className="social-container">
+                <a className="github" href="https://github.com/Monsieur-Nico">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a className="linkedIn" href="https://www.linkedin.com/in/monsieur-nico/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className="twitter" href="https://twitter.com/monsieur_nico">
+                  <ion-icon name="logo-twitter"></ion-icon>
+                </a>
+              </section>
             </li>
           </ul>
         </footer>
