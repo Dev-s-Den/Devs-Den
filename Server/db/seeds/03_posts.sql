b464f61714c6ac9ce738cec50920e70436883698
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-24 18:09:23 -0500

    update seeds

--- Server/db/seeds/03_posts.sql
+++ Server/db/seeds/03_posts.sql
@@ -1,17 +1,5 @@
 INSERT INTO posts (user_id, forum_id, content, img)
- VALUES ('1', '2', '```js
-function test () {
-  for (let test of tests) {
-  Hello there mister
-  }
-return too late mister
-};
- ```
-', 'https://i.imgur.com/fuOFQoH.png'),
-  ('1', '1', 'How do I create a branch and checkout at the same time? ', 'https://i.imgur.com/fuOFQoH.png'), 
-  ('3', '2', 'How did Jose Alex and Nico make this amazing app?', 'https://i.imgur.com/fuOFQoH.png'),
-  ('2', '1', 'I Love Spagheti and meetballs? ', 'https://i.imgur.com/fuOFQoH.png'), 
-  ('3', '1', 'What is the answer to life and the universe', 'https://i.imgur.com/fuOFQoH.png'),
+ VALUES 
   ('1', '7', 'I tried selecting everything in a table but did not work
   ```sql
   SELECT * FROM table
--- Server/db/seeds/04_comments.sql
+++ Server/db/seeds/04_comments.sql
@@ -1,29 +1,27 @@
 INSERT INTO comments (post_id, user_id, content)
- VALUES ('1', '2', 'I have no idea how to help you.'),
-('1', '1', 'This is an amazing question but maybe someone with more expereince can help.'), 
-('2', '3', 'No Way Jose will be able to help you with this'),
-('6', '2', 'You are missing a semicolon
+ VALUES 
+('1', '2', 'You are missing a semicolon
 ```sql
 SELECT * FROM table;
 ```
 '),
-('7', '1', 'The SQL standard way
+('2', '1', 'The SQL standard way
 ```sql
 select column_name, data_type, character_maximum_length, column_default, is_nullable
 from INFORMATION_SCHEMA.COLUMNS where table_name = <name of table>;
 ```
 '),
-('7', '3', 'Try this (in the `psql` command-line tool):
+('2', '3', 'Try this (in the `psql` command-line tool):
 ```sql
 \d+ tablename
 ```
 '),
-('8', '2', 'Set the PGPASSWORD environment variable inside the script before calling psql
+('3', '2', 'Set the PGPASSWORD environment variable inside the script before calling psql
 ```sql
 PGPASSWORD=pass1234 psql -U MyUsername myDatabaseName
 ```
 '),
-('8', '1', 'It is also convenient to have a ~/.pgpass file to avoid regularly having to type in passwords. See [Section 30.13](https://www.postgresql.org/docs/8.3/libpq-pgpass.html) for more information.
+('3', '1', 'It is also convenient to have a ~/.pgpass file to avoid regularly having to type in passwords. See [Section 30.13](https://www.postgresql.org/docs/8.3/libpq-pgpass.html) for more information.
 
 This file should contain lines of the following format:
 ```sql
