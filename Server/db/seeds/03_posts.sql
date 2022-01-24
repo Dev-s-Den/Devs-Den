8814083d5214b2fcbd123d0baca03faa5c5f76c3
Alex Raffa <alex_raffa16@hotmail.com>
2022-01-24 14:54:17 -0500

    seed data with code blocks testing

--- Server/db/seeds/03_posts.sql
+++ Server/db/seeds/03_posts.sql
@@ -1,5 +1,12 @@
 INSERT INTO posts (user_id, forum_id, content, img)
- VALUES ('1', '2', 'Someone please explain how to create methods inside of a ruby object?', 'https://i.imgur.com/fuOFQoH.png'),
+ VALUES ('1', '2', '```js
+ function test () {
+   for (let test of tests){
+     Hello there mister}
+ return too late mister
+ }
+ ```   
+        ', 'https://i.imgur.com/fuOFQoH.png'),
         ('1', '1', 'How do I create a branch and checkout at the same time? ', 'https://i.imgur.com/fuOFQoH.png'), 
         ('3', '2', 'How did Jose Alex and Nico make this amazing app?', 'https://i.imgur.com/fuOFQoH.png'),
         ('2', '1', 'I Love Spagheti and meetballs? ', 'https://i.imgur.com/fuOFQoH.png'), 
