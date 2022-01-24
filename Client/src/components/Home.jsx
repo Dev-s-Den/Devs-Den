762e0751296920ce02d3163a3285aeb2026dc891
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-23 21:02:22 -0500

    Fix key issue for Carousel Item

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Carousel from 'react-bootstrap/Carousel'
-import "./Styles/Home.scss"
-import Tilt from 'react-parallax-tilt';
+import Carousel from "react-bootstrap/Carousel";
+import "./Styles/Home.scss";
+import Tilt from "react-parallax-tilt";
 
 // Components
 import HomePost from "./HomePost";
-
-
 
 export default function Home(props) {
   // States
@@ -22,22 +20,25 @@ export default function Home(props) {
       .catch((err) => console.error(err.message));
   }, []);
 
-
-
   return (
     <Carousel>
       {posts.map((post) => {
         return (
-          <Carousel.Item>
+          <Carousel.Item key={post.id}>
             <div className="body">
-              <Tilt glareEnable={true} glareMaxOpacity={0.25} glareColor="rgba(49, 204, 215, 0.646)" glarePosition="all" glareBorderRadius="25px">
+              <Tilt
+                glareEnable={true}
+                glareMaxOpacity={0.25}
+                glareColor="rgba(49, 204, 215, 0.646)"
+                glarePosition="all"
+                glareBorderRadius="25px"
+              >
                 <HomePost key={post.id} {...post} user={props.user} />
               </Tilt>
             </div>
-          </Carousel.Item>)
+          </Carousel.Item>
+        );
       })}
-    </Carousel >
+    </Carousel>
   );
 }
-
-
