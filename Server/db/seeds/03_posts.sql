00b1d0cff4533ed5275a20a6522e98305fb6d0dd
BRB3D <samuraix221@hotmail.com>
2022-01-24 18:59:58 -0500

    like for post

--- Server/db/seeds/03_posts.sql
+++ Server/db/seeds/03_posts.sql
@@ -5,7 +5,7 @@ INSERT INTO posts (user_id, forum_id, content, img, likes)
   SELECT * FROM table
   ```
   Can someone help?', null, 2),
-  ('2', '7', 'How do you perform the equivalent of Oracles `DESCRIBE TABLE` in PostgreSQL (using the psql command)?', null),
+  ('2', '7', 'How do you perform the equivalent of Oracles `DESCRIBE TABLE` in PostgreSQL (using the psql command)?', null, 4),
   ('3', '7', 'I am trying to automate database creation process with a shell script and one thing I have hit a road block with passing a password to psql. Here is a bit of code from the shell script:
   ```sql
   psql -U $DB_USER -h localhost -c"$DB_RECREATE_SQL"
